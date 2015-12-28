ac011e6c72f6b2e5590f190f989869acb46f565f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-28 14:25:20 -0600

    Add in UserSerializer.

---
+++ accounts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    username = serializers.CharField(
+            max_length=100,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    password = serializers.CharField(min_length=6, max_length=100)
+
+    def create(self, validated_data):
+        user = User(email=validated_data['email'],
+                username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
