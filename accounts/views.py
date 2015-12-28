18e3c27d2747f04e9588c6ed914da6285bc395fc
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-28 17:22:42 -0600

    Return Token with user information.

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from accounts.serializers import UserSerializer
+from rest_framework.authtoken.models import Token
 
 class UserCreate(APIView):
     """ 
@@ -13,6 +14,9 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
