92d5fc4dbdfdc63f12efe0057efe8eaf9fac35c5
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-28 16:11:17 -0600

    Add in tests for password validation.

--- accounts/tests.py
+++ accounts/tests.py
@@ -32,3 +32,32 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.data['email'], data['email'])
         #self.assertEqual(response.data['token'], token.key)
         self.assertFalse('password' in response.data)
+
+
+    def test_create_user_with_short_password(self):
+        """
+        Ensures user is not created for password lengths less than 8.
+        """
+
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'password': 'foo'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+    def test_create_user_with_no_password(self):
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'password': ''
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
