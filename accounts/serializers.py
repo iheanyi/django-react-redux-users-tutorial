53fa3ff20c24f8658a321cab74b475109ffb2d88
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-28 16:43:36 -0600

    Maximum length for usernames is 32, rather than 100.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
     username = serializers.CharField(
-            max_length=100,
+            max_length=32,
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
     password = serializers.CharField(min_length=6, max_length=100,
